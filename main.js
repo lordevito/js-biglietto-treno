c2a03037cfbdd7cc451053c3855e6a17b89355d3
lordevito <lollo.devito@hotmail.com>
2023-11-06 19:57:34 +0100

    feat: file javascript completo

--- main.js
+++ main.js
@@ -1,1 +1,38 @@
 `use strict`;
+
+
+/*Il programma dovrà chiedere all'utente:
+- i km che deve percorrere
+- qunati anni ha*/
+
+const numeriKm = parseInt(prompt (`Inserisci i km`));
+const etàPasseggero = parseInt(prompt (`Inserisci la tua età`));
+console.log (`I tuoi km: `, numeriKm);
+console.log (`Età: `, etàPasseggero);
+
+/*Una volta ricevute queste informazioni, va calcolato il prezzo totale del biglietto sulle seguenti regole:
+- prezzo del biglietto è definito in base ai km (0,21€ al km)
+- sconto 20% per i minorenni
+- sconto 40% per gli over 65
+il totale andrà scritto con due posizioni decimali*/
+
+
+const price = (numeriKm * 0.21);
+
+if (etàPasseggero < 18)
+{
+    let total = (price - (20 * price / 100)).toFixed(2);
+    console.log(`Prezzo totale: `, total);
+}
+else if (etàPasseggero >= 65)
+{
+    let total = (price - (40 * price / 100)).toFixed(2);
+    console.log(`Prezzo totale: `, total);
+}
+
+else{
+    let total = price.toFixed(2);
+    console.log(`Prezzo totale: `, total);
+}
+
+
